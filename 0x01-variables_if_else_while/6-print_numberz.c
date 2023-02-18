a1e8c11295d8f67f24ff32408963609717167db1
tasneem <tasneem126@gmail.com>
2023-02-17 20:16:51 -0800

    6th task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - this main function
+* Return: always 0
+*/
+int main(void)
+{
+char x;
+for (x = '0' ; x <= '9' ; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
