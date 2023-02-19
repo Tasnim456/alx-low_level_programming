b608617597027588dc103614e55a3ea520c7c5c8
tasneem <tasneem126@gmail.com>
2023-02-18 18:19:14 -0800

    11 th task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,12 +14,12 @@ for (b = 0 ; b < 10 ; b++)
 {
 for (c = 0 ; c < 10 ; c++)
 {
-if (a != b && b != c && c < b && b < a)
+if (c != b && b != a && a < b && b < c)
 {
-putchar('0' + c);
-putchar('0' + b);
 putchar('0' + a);
-if (a + b + c != 9 + 8 + 7)
+putchar('0' + b);
+putchar('0' + c);
+if (c + b + a != 9 + 8 + 7)
 {
 putchar(',');
 putchar(' ');
